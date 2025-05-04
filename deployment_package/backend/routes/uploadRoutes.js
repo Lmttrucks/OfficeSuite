ae369f76df2342efde7e474d635a8e64487f5923
shane <shane@TOWER>
2025-05-04 20:32:28 +0100

    removed zip

--- deployment_package/backend/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateToken, authorizeRoles } = require('../middleware/authMiddleware');
-
-router.get('/Upload-info', authenticateToken, authorizeRoles(['admin', 'manager']), (req, res) => {
-    res.json({ message: 'Upload is operational' });
-});
-
-module.exports = router;
